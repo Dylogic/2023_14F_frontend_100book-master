304baed9b5498d25718c59cf746df3ab3dc799e0
Dylo <dylogic06@gmail.com>
2023-12-01 09:44:13 +0100

    Szerző filter hozzáadva

--- src/services/bookservice.js
+++ src/services/bookservice.js
@@ -10,6 +10,27 @@ export default {
             .catch(err => {
                 console.log(err);
             })
+    },
 
+    getAllAuthor(){
+        return axios.get('http://localhost:8000/api/get-all-authors')
+            .then(resp => {
+                // console.log(resp.data);
+                return resp.data;
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    },
+
+    getBooksByAuthor(authorName){
+        return axios.get('http://localhost:8000/api/book-by-author/'+authorName)
+            .then(resp => {
+                // console.log(resp.data);
+                return resp.data;
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 }
